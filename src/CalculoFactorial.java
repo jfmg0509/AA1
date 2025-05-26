f5ed94a98882ebd9ce17e40a9b60f6576c320791
Juan Francisco <jfmg0509@gmail.com>
2025-05-25 20:55:00 -0500

    clase bucle factorial

---
+++ src/CalculoFactorial.java
@@ -0,0 +1,11 @@
+public class CalculoFactorial {
+    //clase para calcular un factorial
+    public long CalculoFactorial(int numero){
+        long resultado = 1;
+        //Bucle "for" que multiplica todos los números para el factorial
+        for (int = 1; i <= numero; i++){
+            resultado *= i;
+            }
+        return resultado;
+    }
+}
